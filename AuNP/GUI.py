4c0c6928bc5847b24a1e8e791010271b2c582c5b
modellingtyro <michaelmarah02@ou.edu>
2025-05-21 14:19:58 -0500

    Update GUI.py

--- AuNP/GUI.py
+++ AuNP/GUI.py
@@ -9,7 +9,7 @@ import webbrowser
 from metrics import calculate_density, compute_nnd, plot_nnd_histogram, plot_3d_scatter
 from visualization import viz_np, apply_mask
 from segmentation import segment_nanoparticles
-from thresholding import apply_threshold
+from thresholding import apply_otsu_threshold
 from preprocessing import preprocess_image
 
 class NanoCount:
