d845723caac6da5a5cef1dc73e376804a0c072c3
modellingtyro <michaelmarah02@ou.edu>
2025-05-21 10:15:13 -0500

    Update main.py

--- AuNP/main.py
+++ AuNP/main.py
@@ -1,9 +1,9 @@
 import tkinter as tk
-from GUI import NanoQuantGUI  # Make sure GUI.py contains class NanoQuantGUI
+from GUI import NanoCount  # Make sure GUI.py contains class NanoCount
 
 def main():
     root = tk.Tk()
-    app = NanoQuantGUI(root)
+    app = NanoCount(root)
     root.mainloop()
 
 if __name__ == "__main__":
